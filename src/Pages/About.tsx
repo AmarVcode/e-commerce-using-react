2fd5b2a50248f37b54e482196a59677d38743a8b
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-02-21 01:20:54 +0530

    updated

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+
+<h1>
+  About
+</h1>
+
+<p>
+  This is a simple E-commerce Website. Last Assigment Of Intershalla React Course. The Course Was Amazing . I learn many Things About React Like What is React , Single Page Web Application, React Hooks, Api, FireBase, React Routing ... etc.
+  Thank You
+</p>
+
+    </div>
   )
 }
 
--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from '../Database/products.json'
+
+
+
 
 const Category = () => {
+  const catdata=localStorage.getItem("catdata");
+  const [productdata,changedata]=useState("");
+  let newdata:any=[];
+
+const mapeddata=()=>{
+  return newdata.map((elem:any)=>{
+    return <div>
+      
+    </div>
+
+    
+  })
+}
+
+  
+for(let i=0;i<data.length;i++){
+if(data[i].categoryId === catdata){
+newdata.push(data[i])
+}
+}
+console.log(newdata)
   return (
     <div>Category</div>
   )
