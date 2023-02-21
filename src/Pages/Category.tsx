be18fb4eb734cfac4ad57df4a6d06c6db41c8cb7
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-02-22 00:30:32 +0530

    updated

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { json } from 'react-router-dom';
 import data from '../Database/products.json'
 
 
@@ -9,10 +10,56 @@ const Category = () => {
   const [productdata,changedata]=useState("");
   let newdata:any=[];
 
+
+const addcart=(d:any)=>{
+
+let local:any=localStorage.getItem("cart")||"[]";
+local=JSON.parse(local);
+
+if(local.length===0){
+  local.push(d)
+}
+else{
+  for(let i=0;i<local.length;i++)
+{
+  if(local[i].id === d.id){
+   break;
+  }
+  else{
+   local.push(d)
+  }
+  
+
+
+}}
+local=JSON.stringify(local);
+localStorage.setItem("cart",local);
+}
+
+
 const mapeddata=()=>{
   return newdata.map((elem:any)=>{
-    return <div>
-      
+    return <div key={elem.id}>
+      <div className="imgdiv">
+        <img src={elem.thumbnail} alt="" />
+      </div>
+      <div className="flex-c">
+
+      <h2>
+        {elem.name}
+      </h2>
+      <h3>
+        Price {elem.price} {elem.currency}
+      </h3>
+      <h4>
+        Delivery {
+          elem.delivery===true?"40rs":"Free"
+        }
+      </h4>
+      <button onClick={()=>{addcart(elem)}}>
+        Add To Cart
+      </button>
+        </div>
     </div>
 
     
@@ -25,9 +72,23 @@ if(data[i].categoryId === catdata){
 newdata.push(data[i])
 }
 }
-console.log(newdata)
   return (
-    <div>Category</div>
+    <div className='grid-cat'>
+      <div>
+        <div className='box'>
+         <input type="checkbox" name="Instock" id="Instock" />
+<label htmlFor="Instock">In Stock</label> 
+        </div>
+        <div className='box'>
+         <input type="checkbox" name="delivery" id="delivery" />
+<label htmlFor="delivery">Free Delivery</label> 
+        </div>
+
+      </div>
+      <div className='cat-data-map'>
+        {mapeddata()}
+      </div>
+    </div>
   )
 }
 
